52103a28ecc13fb22ad693c0d72853941857253e
Milon <hrmilon956@gmail.com>
2025-04-03 11:14:19 +0600

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+  <main>
+    <!-- app.vue always render -->
+    <div class="p-10">
+      Vue + Taiwind + Shadcn Scaffolding with pinia <br>
+      ApiService is ready to use just get your backend api <br>
+      Setup .env file as per instraction. <br>
+      Fake db.json for frontend dev <br>
+
+      <RouterLink class="underline" to="shadcn">Check Shadcn Components</RouterLink>
+
+
+    </div>
+  </main>
+</template>
